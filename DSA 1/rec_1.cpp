9c85b6e0ad27718780d792902b1f5c054b5a065d
kartik0603 <kartikhirapara800@gmail.com>
2024-02-01 11:11:00 +0530

    1/2/2024

---
+++ DSA 1/rec_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+    cout << "Hello World";
+    cout << endl;
+
+
+
+    int a ,b, sum=0;
+
+    cout << " Enter Number A:";
+    cin >> a;
+    cout << endl;
+
+    cout << " Enter Number B:";
+    cin>>b;
+
+    sum = a + b;
+
+    cout << "sum = " <<sum << endl;
+
+
+    return 0;
+}
